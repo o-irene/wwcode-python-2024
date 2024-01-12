e851804644bce79f430472b6adb775ec32677491
iohil <iohil@softserveinc.com>
2024-01-12 19:32:50 +0200

    Completed Day 2 challenge.

---
+++ Day2_CircleArea.py
@@ -0,0 +1,16 @@
+import math
+
+
+def calc_circle_area(radius):
+    pi_number = math.pi
+    area = radius ** 2 * pi_number
+    return area
+
+
+user_radius = input("To find the circle area, print the radius: ")
+
+try:
+    circle_area = calc_circle_area(float(user_radius))
+    print(f"The area of your circle is {circle_area}.")
+except ValueError:
+    print("Oops! To calculate the circle area, please, input only numbers.")
