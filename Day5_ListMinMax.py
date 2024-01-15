1f343176e2613c352ab5683e8cca2f6a1a08f32e
iohil <iohil@softserveinc.com>
2024-01-15 22:05:18 +0200

    Completed Day 5 challenge.

---
+++ Day5_ListMinMax.py
@@ -0,0 +1,21 @@
+# Task: Write a program to find the maximum and minimum values in a list.
+
+def find_extremums(ls):
+    ls = [int(i) for i in ls]
+    min_value = min(ls)
+    max_value = max(ls)
+    return min_value, max_value
+
+
+def main():
+    user_ls = input("Print the numbers, using space as a separator: ").split()
+    try:
+        min_ls, max_ls = find_extremums(user_ls)
+        print(f"The minimum values in your list is {min_ls}, while the maximum number is {max_ls}.")
+    except ValueError:
+        print("Only numbers are accepted. Try again and ensure you do not input any characters other than numbers.\n")
+        main()
+
+
+if __name__ == "__main__":
+    main()
