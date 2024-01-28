f013ed38911f33f0af2ca6aad42daf0a98ff711b
iohil <iohil@softserveinc.com>
2024-01-28 23:31:09 +0200

    Simplified the solution for Day 17.

--- Day17_CapitalizeWords.py
+++ Day17_CapitalizeWords.py
@@ -1,12 +1,6 @@
 # Task: Create a program that capitalizes the first letter of each word in a sentence
 
-def capitalize_words(txt):
-    words_ls = txt.split()
-    words_capitalized = [word.title() for word in words_ls]
-    return ' '.join(words_capitalized)
-
-
 if __name__ == "__main__":
     user_txt = input("Print the sentence for which you want to get each word capitalized: ")
-    user_txt_capitalized = capitalize_words(user_txt)
+    user_txt_capitalized = user_txt.title()
     print(user_txt_capitalized)
