4080c8d8d124140ac8c449b157c1c1cb003ddb2e
iohil <iohil@softserveinc.com>
2024-08-03 23:09:28 +0300

    Added a dummy text in case a user does not input his/her own text.

--- Day17_CapitalizeWords.py
+++ Day17_CapitalizeWords.py
@@ -2,5 +2,7 @@
 
 if __name__ == "__main__":
     user_txt = input("Print the sentence for which you want to get each word capitalized: ")
+    if not user_txt:
+        user_txt = 'You did not provide any text, so I generated this one for you.'
     user_txt_capitalized = user_txt.title()
     print(user_txt_capitalized)
